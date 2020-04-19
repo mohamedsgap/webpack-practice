978ed90f3f785da15a395111edce1f5afccedf8d
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-04-20 01:18:20 +0200

    Update webpack config for html/file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,28 @@ module.exports = {
     module: {
         rules: [
             {
-              test: /\.scss$/,
-              use: ["style-loader", "css-loader", "sass-loader"]
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"],
+
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        esModule: false,
+                        name: "[name].[hash].[ext]",
+                        outputPath: "imgs"
+                    }
+                }
             }
-          ]
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: "./src/template.html"
-    })],
-    
+        ],
+        plugins: [new HtmlWebpackPlugin({
+            template: "./src/template.html"
+        })],
+    }
 }
