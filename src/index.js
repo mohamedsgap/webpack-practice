a216f299ea19870eb9b4b9a0cbd095374040b5e4
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-04-19 03:31:17 +0200

    Update index.js to config webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-alert("Hello from Mohamed and Webpack");
+import { run } from "./app/app";
+import { AlertService } from "./app/alert.service";
+import { ComponentService } from "./app/component.service";
+const alertService = new AlertService();
+const componentService = new ComponentService();
+run(alertService, componentService);
