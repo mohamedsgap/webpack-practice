a04661659032867979a9469b5b8cf924d54e9fa7
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-04-20 00:32:44 +0200

    Add webpack prod

---
+++ webpack.prod.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const config = require("./webpack.config");
+const merge = require("webpack-merge");
+
+
+module.exports = merge(config, {
+    mode: "production",
+    output: {
+      filename: "hello.[contentHash].js",
+      path: path.resolve(__dirname, "helloDist")
+    }
+  });
