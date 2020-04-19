85a5c689db1fc4006955618475f74c1db1cd7bd7
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-04-20 00:35:54 +0200

    Fix a typo

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,7 +3,7 @@ const config = require("./webpack.config");
 const merge = require("webpack-merge");
 
 module.exports = merge(config, {
-    mode: "production",
+    mode: "development",
     output: {
       filename: "hello.[contentHash].js",
       path: path.resolve(__dirname, "helloDist")
