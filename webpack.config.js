617cf033d79f3d7e8f678d3dac68a4fe75bf33db
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-04-20 00:31:46 +0200

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,7 @@
-const path = require("path")
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: "development",
     entry: "./src/index.js",
-    output: {
-        filename: "hello[contentHash].js",
-        path: path.resolve(__dirname, "helloDist")
-    },
     module: {
         rules: [
             {
