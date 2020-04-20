90ded6d883123d162c23289d7bd4be1cb0b5c54a
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-04-20 02:00:57 +0200

    Update webpack dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,29 @@
 const path = require("path")
 const config = require("./webpack.config");
 const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(config, {
     mode: "development",
     output: {
       filename: "[name].bundle.js",
       path: path.resolve(__dirname, "helloDist")
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: "./src/template.html"
+        })
+      ],
+      module: {
+        rules: [
+          {
+            test: /\.scss$/,
+            use: [
+              "style-loader", //3. Inject styles into DOM
+              "css-loader", //2. Turns css into commonjs
+              "sass-loader" //1. Turns sass into css
+            ]
+          }
+        ]
+      }
 });
